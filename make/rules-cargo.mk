d6249fc4e61d260bf64cafc6743e503e8bda6091
Stelios Tsampas <loathingkernel@gmail.com>
2026-01-28 03:39:45 +0200

    build: gate sniper specific rust linker hack

--- make/rules-cargo.mk
+++ make/rules-cargo.mk
@@ -42,5 +42,7 @@ aarch64-unix_CARGO_ARGS := --target $(aarch64-unix_CARGO_TARGET)
 # HACK: forced to gcc-10 because Sniper's (SteamRT) gcc-14 doesn't come with
 # shared libgcc_s.so and cargo/rustc (?) forces shared linking with that lib.
 ifeq ($(TARGET_ARCH),x86_64)
+ifeq ($(TARGET_STEAMRT),sniper)
 	CARGO_LINKERS :=  $(foreach a,$(unix_ARCHS),CARGO_TARGET_$(call toupper,$($(a)-unix_CARGO_TARGET))_LINKER="$($(a)-unix_TARGET)-gcc-10")
+endif
 endif
