188b604f8e3bd1d5a670a41929025ed4be65e761
Stelios Tsampas <loathingkernel@gmail.com>
2026-04-18 00:15:06 +0300

    umu_helper: remove unused variables
    (cherry picked from commit 4c4e0edb45709a939b319ded493aea9cd167bcdb)

--- umu_helper/umu.c
+++ umu_helper/umu.c
@@ -488,7 +488,8 @@ int main(int argc, char *argv[])
 
     if ((sgi = getenv("SteamGameId")))
     {
-        WCHAR path[MAX_PATH], *p;
+        /* Create desktop window in main thread to prevent race with background thread. */
+        GetDesktopWindow();
 
         set_gamingrepair_succeeded(sgi);
 
